980b97300f5d8a9e122ddc00791173fd6c36b20f
w1nyx <vad.com.z@gmail.com>
2022-06-19 23:01:50 +0300

    add gitignore

--- Programming_Courses/Program.cs
+++ Programming_Courses/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("This is the best project i have ever had!");
-Console.WriteLine("My new change");
+Console.WriteLine("My new change"); //amazing job
