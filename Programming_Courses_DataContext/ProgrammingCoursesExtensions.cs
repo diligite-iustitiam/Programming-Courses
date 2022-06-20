5b79dd2ac828b14f98b918c74231918929b42860
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2022-06-20 16:12:15 +0300

    Heloo

---
+++ Programming_Courses_DataContext/ProgrammingCoursesExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore; // UseSqlite
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection
+
+namespace Courses_Shared
+{
+    public static class ProgrammingCoursesContextExtensions
+    {
+        /// <summary>
+        /// Adds NorthwindContext to the specified IServiceCollection. Uses the Sqlite database provider.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="relativePath">Set to override the default of ".."</param>
+        /// <returns>An IServiceCollection that can be used to add more services.</returns>
+        public static IServiceCollection AddCoursesContext(
+          this IServiceCollection services, string relativePath = "..")
+        {
+            string databasePath = Path.Combine(relativePath, "Northwind.db");
+
+            services.AddDbContext<ProgrammingCoursesContext>(options =>
+              options.UseSqlite($"Data Source={databasePath}")
+              .UseLoggerFactory(new ConsoleLoggerFactory())
+            );
+
+            return services;
+        }
+    }
+}
---
+++ Programming_Courses_EntityModels/ProgrammingCourse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Courses_Shared
+{
+    [Table("Programming_Courses")]
+    public partial class ProgrammingCourse
+    {
+        [Key]
+        [Column(TypeName = "INT IDENTITY(1,1)")]
+        public long CourseId { get; set; }
+        [Column(TypeName = "VARCHAR(60)")]
+        public string CourseName { get; set; } = null!;
+        [Column(TypeName = "VARCHAR(60)")]
+        public string Duration { get; set; } = null!;
+        [Column(TypeName = "VARCHAR(1000)")]
+        public string Description { get; set; } = null!;
+        [Column(TypeName = "DECIMAL")]
+        public byte[]? Price { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string? FutureJob { get; set; }
+    }
+}
