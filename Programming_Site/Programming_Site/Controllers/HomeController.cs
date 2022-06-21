dcf185e6b187d8cf561bd00e0b639fa199811075
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2022-06-21 17:14:07 +0300

    whats up guys good evening and shit its like 17:13 pm

---
+++ Programming_Site/Programming_Site/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Programming_Site.Models;
+using System.Diagnostics;
+
+namespace Programming_Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inner_Page()
+        {
+            return View();
+        }
+        public IActionResult Portfolio_Details()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
