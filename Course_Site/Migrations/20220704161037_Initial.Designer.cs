2cec54c5930c5b3c69cb433729441d31b55bcb61
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2022-07-04 19:11:19 +0300

    Psuh

--- Course_Site/Migrations/20220704161037_Initial.Designer.cs
+++ Course_Site/Migrations/20220704161037_Initial.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Course_Site.Migrations
 {
     [DbContext(typeof(Academy))]
-    [Migration("20220704155229_initial")]
-    partial class initial
+    [Migration("20220704161037_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
